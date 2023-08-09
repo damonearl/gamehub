1eef9d36f54f4643766895074aab8a1219141c61
Damon Earl <damonearl@users.noreply.github.com>
2023-08-08 20:02:11 -0600

    build searchinput 2

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
       <SearchInput />
       <ColorModeSwitch />
